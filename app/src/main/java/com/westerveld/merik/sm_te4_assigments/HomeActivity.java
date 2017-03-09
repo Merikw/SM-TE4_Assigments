b8ebea882271222eb533aa9cd75502acc8581ae3
Merik Westerveld <337441@student.fontys.nl>
2017-03-09 10:26:40 +0100

    Colors whoe!

--- app/src/main/java/com/westerveld/merik/sm_te4_assigments/HomeActivity.java
+++ app/src/main/java/com/westerveld/merik/sm_te4_assigments/HomeActivity.java
@@ -1,15 +1,23 @@
 package com.westerveld.merik.sm_te4_assigments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.HorizontalScrollView;
@@ -21,6 +29,7 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,6 +41,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
+import static android.R.attr.bitmap;
+
 public class HomeActivity extends AppCompatActivity {
 
     private String jsonText = "";
@@ -40,17 +51,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Button btnSearch = (Button) findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +88,7 @@ public class HomeActivity extends AppCompatActivity {
         TextView txtTitle = (TextView) findViewById(R.id.txtTitle);
         JSONclass json = new JSONclass(txtTitle);
         EditText txtSearch = (EditText) findViewById(R.id.txtSearch);
+
         try {
             String s = "http://www.omdbapi.com/?s=" + txtSearch.getText();
             s = s.replaceAll("\\s+", "%20");
@@ -98,6 +99,7 @@ public class HomeActivity extends AppCompatActivity {
             ArrayList<Movie> movieList = new ArrayList<>();
             HorizontalScrollView hsv = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
             LinearLayout hsvLL = (LinearLayout) findViewById(R.id.hsvLinearLayout);
+            hsvLL.removeAllViews();
             for (int i = 0; i < jsonArray.length(); i++) {
                 System.out.println(i);
                 JSONObject movie = jsonArray.getJSONObject(i);
@@ -106,10 +108,23 @@ public class HomeActivity extends AppCompatActivity {
                 movieList.add(movieObj);
                 System.out.println(movieObj.getTitle());
 
+                LinearLayout lLayour = new LinearLayout(this.getBaseContext());
+                lLayour.setOrientation(LinearLayout.VERTICAL);
+                LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(900, ViewGroup.LayoutParams.MATCH_PARENT);
+                layoutParams.gravity=Gravity.CENTER;
+
+                TextView txt = new TextView(this);
+                txt.setText(movieObj.getTitle() + " ("+movieObj.getYear()+")");
                 ImageView image = new ImageView(this);
                 image.setPadding(25,25,25,25);
-                new DownloadImageTask(image).execute(movieObj.getPicURL());
-                hsvLL.addView(image, i);
+                Bitmap img = new DownloadImageTask(image).execute(movieObj.getPicURL()).get();
+                lLayour.addView(txt);
+                lLayour.addView(image);
+                txt.setPadding(25, 25, 25 ,25);
+                lLayour.setPadding(25, 25, 25, 25);
+                Palette p  = Palette.from(img).generate();
+                lLayour.setBackgroundColor(p.getLightMutedColor(0) * 2);
+                hsvLL.addView(lLayour);
             }
 //            txtTitle.setText(movieList.get(0).getTitle());
 //            ImageView imgView = (ImageView) findViewById(R.id.imgView);
