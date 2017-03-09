1a04692668cd7bbaa3e7b2e3fb51453ea5109fac
Merik Westerveld <337441@student.fontys.nl>
2017-03-09 15:02:42 +0100

    Created week 2 of the csi app

--- Practicum/SM-TE4_Practicum/app/src/main/java/com/westerveld/merik/sm_te4_practicum/ReportActivity.java
+++ Practicum/SM-TE4_Practicum/app/src/main/java/com/westerveld/merik/sm_te4_practicum/ReportActivity.java
@@ -6,17 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_report);
 
-        final Button button = (Button) findViewById(R.id.btnReport);
+        final Button button = (Button) findViewById(R.id.btnBack);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+                Intent intent = new Intent(ReportActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
