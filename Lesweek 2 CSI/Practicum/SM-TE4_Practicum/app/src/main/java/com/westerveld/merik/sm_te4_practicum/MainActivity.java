b115cc6534df8942af9ff5bd09606ea0e43fcf8a
Merik Westerveld <337441@student.fontys.nl>
2017-03-16 10:53:45 +0100

    CSI miami (styles)

---
+++ Lesweek 2 CSI/Practicum/SM-TE4_Practicum/app/src/main/java/com/westerveld/merik/sm_te4_practicum/MainActivity.java
@@ -0,0 +1,24 @@
+package com.westerveld.merik.sm_te4_practicum;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.btnReport);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
